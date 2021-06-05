4abd9e984637951fe6b4dbb68110756bf45c5c3e
lukevangorder <lukevangorder@gmail.com>
2021-06-05 17:40:09 -0400

    Adds App.js class

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Play from './play/Play.js'
+import Board from './play/Board.js'
+import React from 'react';
+import { connect } from 'react-redux';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Play />
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { pieces: state.pieces, tiles: state.boardTiles };
+};
+
+export default connect(
+  mapStateToProps
+)(Board); 
