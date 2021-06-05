9b977b058b3ed4e73e63f8e94de65370ef2a66b0
lukevangorder <lukevangorder@gmail.com>
2021-06-05 17:39:28 -0400

    Adds Play.js class

---
+++ chess/src/play/Play.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Board from './Board.js'
+
+export default class Play extends Component {
+    render() {
+        return (
+            <div>
+                <Board />
+            </div>
+        )
+    }
+}
